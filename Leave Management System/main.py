e0896ea4769a773368f2a29a1e32b168c8038aa5
ANKIT KUMAR <ankitkumark9626@gmail.com>
2025-07-06 00:20:57 +0530

    Update main.py

--- Leave Management System/main.py
+++ Leave Management System/main.py
@@ -1,5 +1,9 @@
 from app import app
 
+# This is needed for Gunicorn to find the app
+application = app
+
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
